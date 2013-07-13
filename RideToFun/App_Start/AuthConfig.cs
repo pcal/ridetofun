a9971f8d47505abb6a260af0bad19247a0792e46
Andy Abbott <asabbott@yahoo.com>
2013-07-12 23:51:05 -0500

    initial commit

---
+++ RideToFun/App_Start/AuthConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Web.WebPages.OAuth;
+using RideToFun.Models;
+
+namespace RideToFun
+{
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+            // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
+            // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
+
+            //OAuthWebSecurity.RegisterMicrosoftClient(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //OAuthWebSecurity.RegisterTwitterClient(
+            //    consumerKey: "",
+            //    consumerSecret: "");
+
+            OAuthWebSecurity.RegisterFacebookClient(
+                appId: "550575051672585",
+                appSecret: "310a5c4f3d71b82b9a1c3a40095fa282");
+
+            //OAuthWebSecurity.RegisterGoogleClient();
+        }
+    }
+}
